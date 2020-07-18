93924fa81166e67753da1419b580ee7aed63a322
HermersonDev <ne.hermerson@gmail.com>
2020-07-18 09:35:40 -0300

    :construction: create session endpoint

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,10 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/sessions', 'SessionController.store')
+
+Route.group(() => {
+  Route.resource('/teams', 'TeamController')
+
+  Route.post('/invites', 'InviteController.store').middleware('team')
+}).middleware('auth')
