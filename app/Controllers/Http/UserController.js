95966aa019b9384c3bf82ced799f18ae78acf9d4
HermersonDev <ne.hermerson@gmail.com>
2020-07-18 11:37:02 -0300

    :construction: create register user feature to users invited

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const User = use('App/Models/User')
+const Invite = use('App/Models/Invite')
+
+class UserController {
+  async store ({ request, response, auth }) {
+    const data = request.only(['name', 'email', 'password'])
+
+    const inviteQuery = Invite.query().where('email', data.email)
+    const teams = await inviteQuery.pluck('team_id')
+    if (teams.length === 0) {
+      return response.status(401).json({ error: "You're not invited to any team." })
+    }
+
+    const user = await User.create(data)
+    await user.teams().attach(teams)
+
+    await inviteQuery.delete()
+
+    const token = await auth.attempt(data.email, data.password)
+    return token
+  }
+}
+
+module.exports = UserController
