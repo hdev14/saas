9301f48594f6d13355c97738ec59cc8ac912eeb7
HermersonDev <ne.hermerson@gmail.com>
2020-07-17 22:11:11 -0300

    :tada: first commit with models and migrations

---
+++ database/migrations/1595034406720_invite_schema.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class InviteSchema extends Schema {
+  up () {
+    this.create('invites', (table) => {
+      table.increments()
+      table.integer('user_id')
+        .unsigned()
+        .references('user_id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.integer('team_id')
+        .unsigned()
+        .referencs('team_id')
+        .inTable('teams')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('email').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('invites')
+  }
+}
+
+module.exports = InviteSchema
